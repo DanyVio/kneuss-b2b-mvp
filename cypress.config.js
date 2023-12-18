45ba22ec4f42dca39c67e141129966e6f7c68e17
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-12-18 18:40:33 +0100

    inital commit

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  projectId: "",
+  viewportWidth: 1920,
+  viewportHeight: 1280,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: "https://arcmedia:arcmedia123@knuess-b2b.arcmedia.ch/alle-produkte.html",
+    viewportWidth: 1920,
+    viewportHeight: 1280,
+    specPattern: 'cypress/e2e/tests/**/*.cy.{js,jsx,ts,tsx}',
+  },
+  
+  defaultCommandTimeout: 10000,
+  execTimeout: 120000,
+  taskTimeout: 120000,
+  numTestsKeptInMemory: 0
+});
