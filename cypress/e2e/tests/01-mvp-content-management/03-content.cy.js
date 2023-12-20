2720308fe111f3d238be5917aab022bd0f505c9d
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-12-20 17:36:59 +0100

    content page

--- cypress/e2e/tests/01-mvp-content-management/03-content.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/03-content.cy.js
@@ -5,15 +5,6 @@ describe('Content page', function () {
   it('components home page', function () {
     cy.visit(`${config.baseUrl}`);
     cy.get('.right-\[80px\]')
-      .should('be.visible')
-      .click({ force: true })
-      .click({ force: true })
-      .click({ force: true })
-      .click({ force: true });
-    cy.get('.left-\[80px\]')
-      .should('be.visible')
-      .click({ force: true })
-      .click({ force: true })
       .click({ force: true });
   });
 });
