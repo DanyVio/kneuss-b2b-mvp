216d96883f0d69a63c67c95b1b85a17a18798077
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-12-22 15:11:58 +0100

    Wish list done

---
+++ cypress/e2e/tests/02-customers-and-my-account/04-wishlist.cy.js
@@ -0,0 +1,33 @@
+
+let config = Cypress.config();
+
+describe('Shop Registration', function () {
+
+  it('Create a customer', function () {
+    cy.visit(`${config.baseUrl}`);
+    
+    cy.get('#customer-menu > .hidden')
+      .should('be.visible')
+      .click();
+    cy.get('.absolute > [href="https://knuess-b2b.arcmedia.ch/customer/account/index/"]')
+      .should('be.visible')
+      .click();
+    cy.get('#email')
+      .type('test@test.com');
+    cy.get('#pass')
+      .type('Test1234');
+    cy.get('.fieldset > .actions-toolbar > .btn > span')
+      .should('be.visible')
+      .click();
+    cy.get('#customer-menu > .hidden')
+      .should('be.visible')
+      .click();
+    cy.get('.absolute > [href="https://knuess-b2b.arcmedia.ch/wishlist/"]')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(11) > a')
+      .should('be.visible')
+      .click();
+  });
+});
+
