671708834e25dd836a2817bb877a155bd265f0e2
Daniel Viorel <daniel.viorel@arcmedia.ch>
2023-12-22 12:58:48 +0100

    customer login done

--- cypress/e2e/tests/01-mvp-content-management/03-content.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/03-content.cy.js
@@ -4,7 +4,30 @@ describe('Content page', function () {
 
   it('components home page', function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.right-\[80px\]')
+
+    cy.get(':nth-child(1) > a > .absolute')
+      .should('be.visible')
+      .click({ force: true });
+    cy.get('.grid > :nth-child(2) > a > .absolute')
+      .should('be.visible')
+      .click({ force: true });
+    cy.get(':nth-child(3) > a > .absolute')
+      .should('be.visible')
+      .click({ force: true });
+    cy.get(':nth-child(4) > a > .absolute')
+      .should('be.visible')
+      .click({ force: true });
+    cy.get(':nth-child(5) > a > .absolute')
+      .should('be.visible')
+      .click({ force: true });
+    cy.get(':nth-child(6) > a > .absolute')
+      .should('be.visible')
+      .click({ force: true });
+    cy.get(':nth-child(7) > a > .absolute')
+      .should('be.visible')
+      .click({ force: true });
+    cy.get(':nth-child(8) > a > .absolute')
+      .should('be.visible')
       .click({ force: true });
   });
 });
---
+++ cypress/e2e/tests/02-customers-and-my-account/02-customer-login.cy.js
@@ -0,0 +1,56 @@
+let config = Cypress.config();
+
+describe('Shop Registration', function () {
+
+  it('Create a customer', function () {
+    cy.visit(`${config.baseUrl}`);
+    
+    cy.get('#customer-menu > .hidden')
+      .should('be.visible')
+      .click();
+    cy.get('.absolute > [href="https://knuess-b2b.arcmedia.ch/customer/account/index/"]')
+      .should('be.visible')
+      .click();
+    cy.get('#email')
+      .type('test@test.com');
+    cy.get('#pass')
+      .type('Test1234');
+    cy.get('.fieldset > .actions-toolbar > .btn > span')
+      .should('be.visible')
+      .click();
+    cy.get('#customer-menu > .hidden')
+      .should('be.visible')
+      .click();
+    cy.get('.absolute > [href="https://knuess-b2b.arcmedia.ch/customer/account/"]')
+      .should('be.visible')
+      .click();
+    cy.get('.items > :nth-child(2) > a')
+      .should('be.visible')
+      .click();
+    cy.get('.items > :nth-child(3) > a')
+      .should('be.visible')
+      .click();
+    cy.get('.items > :nth-child(4) > a')
+      .should('be.visible')
+      .click();
+    cy.get('.items > :nth-child(5) > a')
+      .should('be.visible')
+      .click();
+    cy.get('.items > :nth-child(6) > a')
+      .should('be.visible')
+      .click();
+    cy.get('.items > :nth-child(7) > a')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(8) > a')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(9) > a')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(11) > a')
+      .should('be.visible')
+      .click();
+  });
+});
+
