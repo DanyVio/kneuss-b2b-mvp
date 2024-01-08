56c27aa9a45381a223b4b7cc1680d64f8eef4b3e
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-08 16:52:09 +0100

    checking the categories and subcategories

--- cypress/e2e/tests/03-catalog-product-and-categories/01-categories.cy.js
+++ cypress/e2e/tests/03-catalog-product-and-categories/01-categories.cy.js
@@ -44,7 +44,47 @@ describe('categories', function () {
     cy.get('.action > .flex')
       .should('be.visible')
       .click();
-    cy.get('.category41 > .font-inter').should('be.visible')
+    cy.get('.category41 > .font-inter')
+      .should('be.visible')
+      .click();
+    cy.get('.store-logo')
+      .should('be.visible')
+      .click();
+    cy.get('.level-0 > [href="https://knuess-b2b.arcmedia.ch/guggeli.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.level-0 > [href="https://knuess-b2b.arcmedia.ch/poulet.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.level-0 > [href="https://knuess-b2b.arcmedia.ch/flugeli.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.level-0 > [href="https://knuess-b2b.arcmedia.ch/schenkel.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.level-0 > [href="https://knuess-b2b.arcmedia.ch/schnitzel.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.level-0 > [href="https://knuess-b2b.arcmedia.ch/spezial-kuche.html"]')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(1) > .filter-options-title > .title')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(2) > .filter-options-title > .title')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(3) > .filter-options-title > .title')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(4) > .filter-options-title > .title')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('top');
+    cy.get('[href="https://knuess-b2b.arcmedia.ch/spezial-kuche/grill.html"] > .subcategory > .relative')
+      .should('be.visible')
+      .click();
+    cy.get('[action="https://knuess-b2b.arcmedia.ch/checkout/cart/add/uenc/%25uenc%25/product/2181/"] > .product > .hover\:shadow-sm')
       .should('be.visible')
       .click();
   });
