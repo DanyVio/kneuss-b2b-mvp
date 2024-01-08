47ec71a5d5a2fd1158982fc88a628ba2221a1d2f
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-08 16:22:29 +0100

    pagination

--- cypress/e2e/tests/03-catalog-product-and-categories/01-categories.cy.js
+++ cypress/e2e/tests/03-catalog-product-and-categories/01-categories.cy.js
@@ -19,13 +19,32 @@ describe('categories', function () {
     cy.get('.fieldset > .actions-toolbar > .btn > span')
       .should('be.visible')
       .click();
-    cy.get('.order-2 > .w-full > .flex-wrap')
-      .should('be.visible')
-      .click();
     cy.get('.level-0 > [href="https://knuess-b2b.arcmedia.ch/alle-produkte.html"]')
       .should('be.visible')
       .click();
-    cy.get('.border-secondary > .level-0 > :nth-child(1)')
+    cy.get(':nth-child(1) > .filter-options-title')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.get(':nth-child(2) > .filter-options-title')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.get(':nth-child(3) > .filter-options-title')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.scrollTo('bottom');
+    cy.get('.z-0 > :nth-child(3) > .page > :nth-child(2)')
+      .should('be.visible')
+      .click();
+    cy.get('.next')
+      .should('be.visible')
+      .click();
+    cy.get('.action > .flex')
+      .should('be.visible')
+      .click();
+    cy.get('.category41 > .font-inter').should('be.visible')
       .should('be.visible')
       .click();
   });
