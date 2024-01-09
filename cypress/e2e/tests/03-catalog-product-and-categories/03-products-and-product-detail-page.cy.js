af4f041179ef2af258f52963d9198e8d0203fbac
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-09 16:16:40 +0100

    product detail page

---
+++ cypress/e2e/tests/03-catalog-product-and-categories/03-products-and-product-detail-page.cy.js
@@ -0,0 +1,25 @@
+let config = Cypress.config();
+
+describe('categories', function () {
+
+  it('categories', function () {
+    cy.visit(`${config.baseUrl}`);
+
+    cy.get('#customer-menu > .hidden')
+      .should('be.visible')
+      .click();
+    cy.get('.absolute > [href="https://knuess-b2b.arcmedia.ch/customer/account/index/"]')
+      .should('be.visible')
+      .click();
+    cy.get('#email')
+      .type('test@test.com');
+    cy.get('#pass')
+      .type('Test1234');
+    cy.get('.fieldset > .actions-toolbar > .btn > span')
+      .should('be.visible')
+      .click();
+    cy.get('.level-0 > [href="https://knuess-b2b.arcmedia.ch/alle-produkte.html"]')
+      .should('be.visible')
+      .click();
+  });
+});
