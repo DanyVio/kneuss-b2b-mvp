791a3c71d2db5c5e37ee6fa861eb734227ae48d8
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-10 16:31:05 +0100

    doofinder  search

---
+++ cypress/e2e/tests/03-catalog-product-and-categories/04-doofinder-search-engine.cy.js
@@ -0,0 +1,30 @@
+let config = Cypress.config();
+
+describe('categories', function () {
+
+  it('categories', function () {
+    cy.visit(`${config.baseUrl}`);
+
+    cy.get('#customer-menu > .hidden')
+      .should('be.visible')
+      .click();
+    cy.get('.absolute > [href="https://knuess-b2b.arcmedia.ch/customer/account/index/"]')
+      .should('be.visible')
+      .click();
+    cy.get('#email')
+      .type('test@test.com');
+    cy.get('#pass')
+      .type('Test1234');
+    cy.get('.fieldset > .actions-toolbar > .btn > span')
+      .should('be.visible')
+      .click();
+    cy.get('#search-content-desktop > .search-form > #search_mini_form > #search')
+      .type('testingb2b')
+      .click();
+  });
+});
+
+
+
+
+
