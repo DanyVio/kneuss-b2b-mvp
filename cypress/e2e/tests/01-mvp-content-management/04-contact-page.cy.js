d38caa8e29c56aea4f025a4d3ac5a91a37178ed1
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-16 14:22:02 +0100

    contact page

--- cypress/e2e/tests/01-mvp-content-management/04-contact-page.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/04-contact-page.cy.js
@@ -1,8 +1,33 @@
 let config = Cypress.config();
 
-describe('Content page', function () {
 
-  it('components home page', function () {
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+});
+
+describe('Contact page', function () {
+
+  beforeEach('before each test', function () {
     cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+    cy.wait(3000);
+  });
+  it('contact page', function () {
+    cy.visit(`${config.baseUrl}/contact`);
+    cy.get('#name')
+      .type('test');
+    cy.get('#email')
+      .type('test@test.com');
+    cy.get('#telephone')
+      .type('987878676756');
+    cy.get('#comment')
+      .type('testing');
+    cy.get('#contact > .mt-6 > .btn')
+      .should('be.visible')
+      .click();
+    cy.get('.message')
+      .should('be.visible');
   });
 });
