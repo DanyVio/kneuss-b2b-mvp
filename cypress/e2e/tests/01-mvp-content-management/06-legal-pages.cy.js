7ceabdbcf4b142baa69e9d4695ede61934da4f3a
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-16 16:00:39 +0100

    faqs page

--- cypress/e2e/tests/01-mvp-content-management/06-legal-pages.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/06-legal-pages.cy.js
@@ -1,12 +1,36 @@
 let config = Cypress.config();
 
-describe('Legal Pages', function () {
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+});
+
+describe('legal pages', function () {
+
+  beforeEach('before each test', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+    cy.wait(3000);
+  });
 
   it('legal pages', function () {
     cy.visit(`${config.baseUrl}`);
     cy.scrollTo('bottom');
-    cy.get('.copyright > :nth-child(2)')
+    cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/agb/"]')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('bottom');
+    cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/impressum/"]')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('bottom');
+    cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/enable-cookies/"]')
+      .should('be.visible')
       .click();
     cy.scrollTo('top');
+    cy.get('.store-logo')
+      .should('be.visible')
+      .click();
   });
 });
--- cypress/e2e/tests/01-mvp-content-management/07-faq.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/07-faq.cy.js
@@ -1,12 +1,36 @@
 let config = Cypress.config();
 
-describe('FAQ', function () {
+Cypress.on('uncaught:exception', (err, runnable) => {
+  return false
+});
+
+describe('faq pages', function () {
+
+  beforeEach('before each test', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+    cy.wait(3000);
+  });
 
   it('faq page', function () {
-    cy.visit(`${config.baseUrl}`);
-    cy.scrollTo('bottom');
-    cy.get('.copyright > :nth-child(3)')
+    cy.visit(`${config.baseUrl}/faqs`);
+    cy.get('.mgz-panel-first > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon')
+      .should('be.visible')
+      .click()
       .click();
-    cy.scrollTo('top');
+    cy.get(':nth-child(2) > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.get(':nth-child(3) > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.get('.mgz-panel-last > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span')
+      .should('be.visible')
+      .click()
+      .click();
   });
 });
