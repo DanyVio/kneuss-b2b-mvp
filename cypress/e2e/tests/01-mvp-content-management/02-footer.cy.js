ba069c3637e53373a6df3de9e52ab47d785f338d
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-16 12:53:47 +0100

    footer finished

--- cypress/e2e/tests/01-mvp-content-management/02-footer.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/02-footer.cy.js
@@ -6,19 +6,65 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 
 describe('Check the Footer', function () {
 
+  beforeEach('before each test', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+    cy.wait(3000);
+  });
+
   it('footer', function () {
     cy.visit(`${config.baseUrl}`);
     cy.scrollTo('bottom');
-    cy.get(':nth-child(1) > .footer > :nth-child(1) > a')
+    cy.get('.nav.item > [href="https://test-b2b-kneuss.arcmedia.ch/produkte.html"]')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('bottom');
+    cy.get('.nav.item > [href="https://test-b2b-kneuss.arcmedia.ch/guggeli.html"]')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('bottom');
+    cy.get('.nav.item > [href="https://test-b2b-kneuss.arcmedia.ch/schnitzel.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.nav.item > [href="https://test-b2b-kneuss.arcmedia.ch/schenkel.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.nav.item > [href="https://test-b2b-kneuss.arcmedia.ch/flugeli.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.nav.item > [href="https://test-b2b-kneuss.arcmedia.ch/poulet.html"]')
+      .should('be.visible')
+      .click();
+    cy.get('.nav.item > [href="https://test-b2b-kneuss.arcmedia.ch/spezial-kuchen.html"]')
       .should('be.visible')
       .click();
     cy.get('.text-primary-lighter > a')
       .should('be.visible')
       .click();
-    cy.get(':nth-child(2) > .footer > :nth-child(3) > a')
+    cy.get(':nth-child(2) > .footer > :nth-child(2) > a')
       .should('be.visible')
       .click();
-    cy.get('.copyright > :nth-child(2)')
+    cy.get('#newsletter-subscribe')
+      .type('test@test.com');
+    cy.get('.shrink-0')
+      .should('be.visible')
+      .click();
+    cy.get('.message')
+      .should('be.visible');
+    cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/agb/"]')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('bottom');
+    cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/impressum/"]')
+      .should('be.visible')
+      .click();
+    cy.scrollTo('bottom');
+    cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/enable-cookies/"]')
+      .should('be.visible')
+      .click();
+    cy.get('.store-logo')
       .should('be.visible')
       .click();
   });
