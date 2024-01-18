228b8f638924977c3570b19f0bd5949bf31f8c41
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-18 10:58:43 +0100

    wishlist done

--- cypress/e2e/tests/02-customers-and-my-account/04-wishlist.cy.js
+++ cypress/e2e/tests/02-customers-and-my-account/04-wishlist.cy.js
@@ -15,7 +15,29 @@ describe('Wishlist', function () {
     cy.wait(3000);
   });
 
-  it('wishlist', function () {
+  it('wishlist button for products', function () {
+    cy.visit(`${config.baseUrl}/customer/account/login/`);
+    cy.get('#email')
+      .type('test@test.com');
+    cy.get('#pass')
+      .type('Test1234');
+    cy.get('.fieldset > .actions-toolbar > .btn > span')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(1) > .level-0 > span')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(1) > .item > .product')
+      .should('be.visible')
+      .click();
+    cy.get('#add-to-wishlist')
+      .should('be.visible')
+      .click();
+    cy.get('.message')
+      .should('be.visible');
+  });
+
+  it('wishlist my account', function () {
     cy.visit(`${config.baseUrl}/customer/account/login/`);
     cy.get('#email')
       .type('test@test.com');
@@ -28,6 +50,64 @@ describe('Wishlist', function () {
       .should('be.visible')
       .click();
     cy.get('.secondary > .action > span')
+      .should('be.visible')
+      .click();
+      cy.get(':nth-child(1) > .level-0 > span')
+  });
+
+  it('remove products from the wishlist', function () {
+    cy.visit(`${config.baseUrl}/customer/account/login/`);
+    cy.get('#email')
+      .type('test@test.com');
+    cy.get('#pass')
+      .type('Test1234');
+    cy.get('.fieldset > .actions-toolbar > .btn > span')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(1) > .level-0 > span')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(1) > .item > .product')
+      .should('be.visible')
+      .click();
+    cy.get('#add-to-wishlist')
+      .should('be.visible')
+      .click();
+    cy.get('.btn-remove')
+      .should('be.visible')
+      .click();
+    cy.get('.message')
+      .should('be.visible');
+  });
+
+
+  it('move products from my wishlist to cart', function () {
+    cy.visit(`${config.baseUrl}/customer/account/login/`);
+    cy.get('#email')
+      .type('test@test.com');
+    cy.get('#pass')
+      .type('Test1234');
+    cy.get('.fieldset > .actions-toolbar > .btn > span')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(1) > .level-0 > span')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(1) > .item > .product')
+      .should('be.visible')
+      .click();
+    cy.get('#add-to-wishlist')
+      .should('be.visible')
+      .click();
+    cy.get('.message')
+      .should('be.visible');
+    cy.get('.product-item-actions > .actions-primary > .action')
+      .should('be.visible')
+      .click();
+    cy.get('.cart-icon > .hidden')
+      .should('be.visible')
+      .click();
+    cy.get(':nth-child(3) > :nth-child(2) > .underline')
       .should('be.visible')
       .click();
   });
