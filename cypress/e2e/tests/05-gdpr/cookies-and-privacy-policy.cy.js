e5bc9027a48f985c61acf3af7a4318f4340e6f47
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-01-22 12:03:11 +0100

    fine adjustmrnts on cookies

--- cypress/e2e/tests/05-gdpr/cookies-and-privacy-policy.cy.js
+++ cypress/e2e/tests/05-gdpr/cookies-and-privacy-policy.cy.js
@@ -4,19 +4,67 @@ Cypress.on('uncaught:exception', (err, runnable) => {
   return false
 });
 
-describe('set minimum order amount', function () {
+describe('Cookies', function () {
 
-  it('set minimum order amount', function () {
+  it('I can see a cookie banner', function () {
     cy.visit(`${config.baseUrl}`);
     cy.get('.ambar-btn-settings')
       .should('be.visible')
       .click();
-    cy.get('.flex-col > :nth-child(2) > .btn')
+    cy.get(':nth-child(4) > .ambar-action-btn')
       .should('be.visible')
       .click();
-    cy.get('button.absolute')
+  });
+  
+  it('Im able to decline all cookies', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-settings')
       .should('be.visible')
-      .click({ force: true });
+      .click();
+    cy.get('#amcookie-classic-group-2')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.get('#amcookie-classic-group-3')
+      .should('be.visible')
+      .click()
+      .click();
+  });
+
+  it('Im able to accept all cookies', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-settings')
+      .should('be.visible')
+      .click();
+    cy.get('#amcookie-classic-group-2')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.get('#amcookie-classic-group-3')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.get(':nth-child(4) > .ambar-action-btn')
+      .should('be.visible')
+      .click();
+    cy.get('.ambar-btn-accept')
+      .should('be.visible')
+      .click();
+  });
+
+  it('Im can see/accept the privacy policy by checkboxes', function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get('.ambar-btn-settings')
+      .should('be.visible')
+      .click();
+    cy.get('#amcookie-classic-group-2')
+      .should('be.visible')
+      .click()
+      .click();
+    cy.get('#amcookie-classic-group-3')
+      .should('be.visible')
+      .click()
+      .click();
     cy.get(':nth-child(4) > .ambar-action-btn')
       .should('be.visible')
       .click();
