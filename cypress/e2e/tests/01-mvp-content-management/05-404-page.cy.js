2ae2df681908908440ecaa3dfd14a9b7ad6a5098
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-17 11:08:43 +0200

    update 404 page

--- cypress/e2e/tests/01-mvp-content-management/05-404-page.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/05-404-page.cy.js
@@ -1,24 +1,21 @@
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('404 page', function () {
-
-  beforeEach('before each test', function () {
+describe("404 page", function () {
+  beforeEach("before each test", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
     cy.wait(3000);
   });
 
-  it('404 page', function () {
-    cy.visit(`${config.baseUrl}/cuatrocientoscuatro`, { failOnStatusCode: false });
+  it("404 page", function () {
+    cy.visit(`${config.baseUrl}/cuatrocientoscuatro`, {
+      failOnStatusCode: false,
+    });
     cy.wait(4000);
-    cy.get('p > .btn')
-      .should('be.visible')
-      .click();
+    cy.get("p > .btn").should("be.visible").click();
   });
 });
