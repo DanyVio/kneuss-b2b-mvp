0730cfd0354c0f227fde0b0c151c6db7f9e90cf6
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-17 13:06:05 +0200

    updating cookies

--- cypress/e2e/tests/05-gdpr/cookies-and-privacy-policy.cy.js
+++ cypress/e2e/tests/05-gdpr/cookies-and-privacy-policy.cy.js
@@ -1,73 +1,37 @@
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('Cookies', function () {
-
-  it('I can see a cookie banner', function () {
+describe("Cookies", function () {
+  it("I can see a cookie banner", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-settings')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(4) > .ambar-action-btn')
-      .should('be.visible')
-      .click();
-  });
-  
-  it('Im able to decline all cookies', function () {
-    cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-settings')
-      .should('be.visible')
-      .click();
-    cy.get('#amcookie-classic-group-2')
-      .should('be.visible')
-      .click()
-      .click();
-    cy.get('#amcookie-classic-group-3')
-      .should('be.visible')
-      .click()
-      .click();
+    cy.get(".ambar-btn-settings").should("be.visible").click();
+    cy.get(":nth-child(4) > .ambar-action-btn").should("be.visible").click();
   });
 
-  it('Im able to accept all cookies', function () {
+  it("Im able to decline all cookies", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-settings')
-      .should('be.visible')
-      .click();
-    cy.get('#amcookie-classic-group-2')
-      .should('be.visible')
-      .click()
-      .click();
-    cy.get('#amcookie-classic-group-3')
-      .should('be.visible')
-      .click()
-      .click();
-    cy.get(':nth-child(4) > .ambar-action-btn')
-      .should('be.visible')
-      .click();
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-settings").should("be.visible").click();
+    cy.get("#amcookie-classic-group-2").should("be.visible").click().click();
+    cy.get("#amcookie-classic-group-3").should("be.visible").click().click();
   });
 
-  it('Im can see/accept the privacy policy by checkboxes', function () {
+  it("Im able to accept all cookies", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-settings')
-      .should('be.visible')
-      .click();
-    cy.get('#amcookie-classic-group-2')
-      .should('be.visible')
-      .click()
-      .click();
-    cy.get('#amcookie-classic-group-3')
-      .should('be.visible')
-      .click()
-      .click();
-    cy.get(':nth-child(4) > .ambar-action-btn')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-settings").should("be.visible").click();
+    cy.get("#amcookie-classic-group-2").should("be.visible").click().click();
+    cy.get("#amcookie-classic-group-3").should("be.visible").click().click();
+    cy.get(":nth-child(4) > .ambar-action-btn").should("be.visible").click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
+  });
+
+  it("Im can see/accept the privacy policy by checkboxes", function () {
+    cy.visit(`${config.baseUrl}`);
+    cy.get(".ambar-btn-settings").should("be.visible").click();
+    cy.get("#amcookie-classic-group-2").should("be.visible").click().click();
+    cy.get("#amcookie-classic-group-3").should("be.visible").click().click();
+    cy.get(":nth-child(4) > .ambar-action-btn").should("be.visible").click();
   });
 });
-
