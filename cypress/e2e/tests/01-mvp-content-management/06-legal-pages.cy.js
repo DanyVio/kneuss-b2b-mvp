58e6d72feddc82062c458f6776f387fe576cdd63
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-17 11:10:36 +0200

    update legal pages

--- cypress/e2e/tests/01-mvp-content-management/06-legal-pages.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/06-legal-pages.cy.js
@@ -1,36 +1,31 @@
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('legal pages', function () {
-
-  beforeEach('before each test', function () {
+describe("legal pages", function () {
+  beforeEach("before each test", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
     cy.wait(3000);
   });
 
-  it('legal pages', function () {
+  it("legal pages", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.scrollTo('bottom');
+    cy.scrollTo("bottom");
     cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/agb/"]')
-      .should('be.visible')
+      .should("be.visible")
       .click();
-    cy.scrollTo('bottom');
+    cy.scrollTo("bottom");
     cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/impressum/"]')
-      .should('be.visible')
+      .should("be.visible")
       .click();
-    cy.scrollTo('bottom');
+    cy.scrollTo("bottom");
     cy.get('[href="https://test-b2b-kneuss.arcmedia.ch/enable-cookies/"]')
-      .should('be.visible')
+      .should("be.visible")
       .click();
-    cy.scrollTo('top');
-    cy.get('.store-logo')
-      .should('be.visible')
-      .click();
+    cy.scrollTo("top");
+    cy.get(".store-logo").should("be.visible").click();
   });
 });
--- cypress/e2e/tests/01-mvp-content-management/07-faq.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/07-faq.cy.js
@@ -1,35 +1,40 @@
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('faq pages', function () {
-
-  beforeEach('before each test', function () {
+describe("faq pages", function () {
+  beforeEach("before each test", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
     cy.wait(3000);
   });
 
-  it('faq page', function () {
+  it("faq page", function () {
     cy.visit(`${config.baseUrl}/faqs`);
-    cy.get('.mgz-panel-first > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon')
-      .should('be.visible')
+    cy.get(
+      ".mgz-panel-first > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon"
+    )
+      .should("be.visible")
       .click()
       .click();
-    cy.get(':nth-child(2) > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span')
-      .should('be.visible')
+    cy.get(
+      ":nth-child(2) > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span"
+    )
+      .should("be.visible")
       .click()
       .click();
-    cy.get(':nth-child(3) > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span')
-      .should('be.visible')
+    cy.get(
+      ":nth-child(3) > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span"
+    )
+      .should("be.visible")
       .click()
       .click();
-    cy.get('.mgz-panel-last > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span')
-      .should('be.visible')
+    cy.get(
+      ".mgz-panel-last > .mgz-panel-heading > .mgz-panel-heading-title > .has-icon > span"
+    )
+      .should("be.visible")
       .click()
       .click();
   });
