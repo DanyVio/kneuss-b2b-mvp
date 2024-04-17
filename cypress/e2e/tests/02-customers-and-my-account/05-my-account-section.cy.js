09ee9c7ffc7c634a5c6b8da756b260461b6f4712
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-17 12:14:20 +0200

    update my account section

--- cypress/e2e/tests/02-customers-and-my-account/05-my-account-section.cy.js
+++ cypress/e2e/tests/02-customers-and-my-account/05-my-account-section.cy.js
@@ -1,59 +1,30 @@
-
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('My Account', function () {
-
-  beforeEach('before each test', function () {
+describe("My Account", function () {
+  beforeEach("before each test", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
     cy.wait(3000);
   });
 
-  it('my account', function () {
+  it("my account", function () {
     cy.visit(`${config.baseUrl}customer/account/login/`);
-    cy.get('#email')
-      .type('test@test.com');
-    cy.get('#pass')
-      .type('Test1234');
-    cy.get('.fieldset > .actions-toolbar > .btn > span')
-      .should('be.visible')
+    cy.get("#email").type("test@test.com");
+    cy.get("#pass").type("Test1234");
+    cy.get(".fieldset > .actions-toolbar > .btn > span")
+      .should("be.visible")
       .click();
-      cy.get('.items > :nth-child(2) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(3) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(4) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(5) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(6) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(7) > a')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(8) > a')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(9) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.current > strong')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(12) > a')
-      .should('be.visible')
-      .click();
+      cy.get('.current > strong').should("be.visible").click();
+    cy.get(".items > :nth-child(2) > a").should("be.visible").click();
+    cy.get(".items > :nth-child(3) > a").should("be.visible").click();
+    cy.get(".items > :nth-child(4) > a").should("be.visible").click();
+    cy.get(".items > :nth-child(5) > a").should("be.visible").click();
+    cy.get(".items > :nth-child(6) > a").should("be.visible").click();
+    cy.get(':nth-child(9) > a').should("be.visible").click();
+    cy.get(':nth-child(8) > a').should("be.visible").click();
   });
 });
-
