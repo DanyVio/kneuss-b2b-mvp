07b03a54420600e4302b406e0418997581440102
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-17 10:21:55 +0200

    update header

--- cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
+++ cypress/e2e/tests/01-mvp-content-management/01-header.cy.js
@@ -1,59 +1,44 @@
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('Check the Header', function () {
-
-  beforeEach('before each test', function () {
+describe("Check the Header", function () {
+  beforeEach("before each test", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
     cy.wait(3000);
   });
 
-  it('header', function () {
+  it("header", function () {
     cy.visit(`${config.baseUrl}`);
-
-    cy.get('.level-0 > [href="https://test-b2b-kneuss.arcmedia.ch/produkte.html"]')
-      .should('be.visible')
+    cy.get(":nth-child(1) > .level-0 > span").should("be.visible").click();
+    cy.get(":nth-child(2) > .level-0 > :nth-child(1)")
+      .should("be.visible")
       .click();
-    cy.get('.level-0 > [href="https://test-b2b-kneuss.arcmedia.ch/guggeli.html"]')
-      .should('be.visible')
+    cy.get(".flex-wrap > :nth-child(3) > .level-0 > :nth-child(1)")
+      .should("be.visible")
       .click();
-    cy.get('.level-0 > [href="https://test-b2b-kneuss.arcmedia.ch/schnitzel.html"]')
-      .should('be.visible')
+    cy.get(":nth-child(4) > .level-0 > :nth-child(1)")
+      .should("be.visible")
       .click();
-    cy.get('.level-0 > [href="https://test-b2b-kneuss.arcmedia.ch/schenkel.html"]')
-      .should('be.visible')
+    cy.get(".border-secondary > .level-0 > :nth-child(1)")
+      .should("be.visible")
       .click();
-    cy.get('.level-0 > [href="https://test-b2b-kneuss.arcmedia.ch/flugeli.html"]')
-      .should('be.visible')
+    cy.get(":nth-child(6) > .level-0 > :nth-child(1)")
+      .should("be.visible")
       .click();
-    cy.get('.level-0 > [href="https://test-b2b-kneuss.arcmedia.ch/poulet.html"]')
-      .should('be.visible')
+    cy.get(":nth-child(7) > .level-0 > :nth-child(1)")
+      .should("be.visible")
       .click();
-    cy.get('.level-0 > [href="https://test-b2b-kneuss.arcmedia.ch/spezial-kuchen.html"]')
-      .should('be.visible')
-      .click();
-    cy.get('.store-logo')
-      .should('be.visible')
-      .click();
-    cy.get('#search-content-desktop > .search-form > #search_mini_form > #search')
-      .type('poulet');
-    cy.get('.dfd-close-button')
-      .should('be.visible')
-      .click();
-    cy.get('.dfd-logo')
-      .should('be.visible')
-      .click();
-    cy.get('#customer-menu')
-      .should('be.visible')
-      .click();
-    cy.get('.store-logo')
-      .should('be.visible')
-      .click();
+    cy.get(".store-logo").should("be.visible").click();
+    cy.get(
+      "#search-content-desktop > .search-form > #search_mini_form > #search"
+    ).type("poulet");
+    cy.get(".dfd-close-button").should("be.visible").click();
+    cy.get(".dfd-logo").should("be.visible").click();
+    cy.get("#customer-menu").should("be.visible").click();
+    cy.get(".store-logo").should("be.visible").click();
   });
 });
--- cypress/e2e/tests/04-checkout-process/05-shopping-cart-persistancy.cy.js
+++ cypress/e2e/tests/04-checkout-process/05-shopping-cart-persistancy.cy.js
@@ -1,63 +1,43 @@
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('Shopping cart persistancy', function () {
-
-  beforeEach('before each test', function () {
+describe("Shopping cart persistancy", function () {
+  beforeEach("before each test", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
     cy.wait(3000);
   });
 
-  it('shopping cart persistancy', function () {
+  it("shopping cart persistancy", function () {
     cy.visit(`${config.baseUrl}customer/account/login/`);
-    cy.get('#email')
-      .type('test@test.com');
-    cy.get('#pass')
-      .type('Test1234');
-    cy.get('.fieldset > .actions-toolbar > .btn > span')
-      .should('be.visible')
+    cy.get("#email").type("test@test.com");
+    cy.get("#pass").type("Test1234");
+    cy.get(".fieldset > .actions-toolbar > .btn > span")
+      .should("be.visible")
       .click();
-    cy.get(':nth-child(1) > .level-0 > span')
-      .should('be.visible')
+    cy.get(":nth-child(1) > .level-0 > span").should("be.visible").click();
+    cy.get(":nth-child(2) > .item > .product-info > .btn-container > .btn")
+      .should("be.visible")
       .click();
-    cy.get(':nth-child(2) > .item > .product-info > .btn-container > .btn')
-      .should('be.visible')
-      .click();
-    cy.get('.cart-icon > .hidden')
-      .should('be.visible')
-      .click();
-    cy.get('.flex-col > .btn-secondary')
-      .should('be.visible')
-      .click();
+    cy.get(".cart-icon > .hidden").should("be.visible").click();
+    cy.get(".flex-col > .btn-secondary").should("be.visible").click();
   });
 
-  it('checking the cart', function () {
+  it("checking the cart", function () {
     cy.visit(`${config.baseUrl}customer/account/login/`);
-    cy.get('#email')
-      .type('test@test.com');
-    cy.get('#pass')
-      .type('Test1234');
-    cy.get('.fieldset > .actions-toolbar > .btn > span')
-      .should('be.visible')
+    cy.get("#email").type("test@test.com");
+    cy.get("#pass").type("Test1234");
+    cy.get(".fieldset > .actions-toolbar > .btn > span")
+      .should("be.visible")
       .click();
-    cy.get(':nth-child(1) > .level-0 > span')
-      .should('be.visible')
+    cy.get(":nth-child(1) > .level-0 > span").should("be.visible").click();
+    cy.get(":nth-child(2) > .item > .product-info > .btn-container > .btn")
+      .should("be.visible")
       .click();
-    cy.get(':nth-child(2) > .item > .product-info > .btn-container > .btn')
-      .should('be.visible')
-      .click();
-    cy.get('.cart-icon > .hidden')
-      .should('be.visible')
-      .click();
-    cy.get('.flex-col > .btn-secondary')
-      .should('be.visible')
-      .click();
+    cy.get(".cart-icon > .hidden").should("be.visible").click();
+    cy.get(".flex-col > .btn-secondary").should("be.visible").click();
   });
 });
-
