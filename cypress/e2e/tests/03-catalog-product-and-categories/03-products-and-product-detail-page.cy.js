b2fc397a9719ca80251901dbceca53f0530b1fb7
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-17 12:54:30 +0200

    update pdp

--- cypress/e2e/tests/03-catalog-product-and-categories/03-products-and-product-detail-page.cy.js
+++ cypress/e2e/tests/03-catalog-product-and-categories/03-products-and-product-detail-page.cy.js
@@ -1,76 +1,42 @@
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('Product detail page', function () {
-
-  beforeEach('before each test', function () {
+describe("Product detail page", function () {
+  beforeEach("before each test", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
     cy.wait(3000);
   });
 
-  it('product detail page as a registered user', function () {
+  it("product detail page as a registered user", function () {
     cy.visit(`${config.baseUrl}customer/account/login/`);
-    cy.get('#email')
-      .type('test@test.com');
-    cy.get('#pass')
-      .type('Test1234');
-    cy.get('.fieldset > .actions-toolbar > .btn > span')
-      .should('be.visible')
+    cy.get("#email").type("test@test.com");
+    cy.get("#pass").type("Test1234");
+    cy.get(".fieldset > .actions-toolbar > .btn > span")
+      .should("be.visible")
       .click();
-    cy.get(':nth-child(1) > .level-0 > span')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(2) > .item > .product')
-      .should('be.visible')
-      .click();
-    cy.get('.outline-offset-2')
-      .should('be.visible')
-      .click();
-    cy.get('.text-gray-500')
-      .should('be.visible')
-      .click();
-    cy.get('#add-to-wishlist')
-      .should('be.visible')
-      .click();
-    cy.get('.secondary > .action > span')
-      .should('be.visible')
-      .click();
-    cy.get('.actions-primary > .action')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(12) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.store-logo')
-      .should('be.visible')
-      .click();
+    cy.get(":nth-child(1) > .level-0 > span").should("be.visible").click();
+    cy.get(":nth-child(2) > .item > .product").should("be.visible").click();
+    cy.get(".outline-offset-2").should("be.visible").click();
+    cy.get(".text-gray-500").should("be.visible").click();
+    cy.get("#add-to-wishlist").should("be.visible").click();
+    cy.get(".secondary > .action > span").should("be.visible").click();
+    cy.get(".actions-primary > .action").should("be.visible").click();
+    cy.get(".store-logo").should("be.visible").click();
   });
 
-  it('as a not registered user', function () {
+  it("as a not registered user", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get(':nth-child(1) > .level-0 > span')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(2) > .item > .product')
-      .should('be.visible')
-      .click();
-    cy.get('.outline-offset-2')
-      .should('be.visible')
-      .click();
-    cy.get('.text-gray-500')
-      .should('be.visible')
-      .click();
-    cy.get('.product-data > .action')
-      .should('be.visible')
-      .click();
-    cy.get('#customer-login-form > .fieldset > .actions-toolbar > .btn')
-      .should('be.visible')
+    cy.get(":nth-child(1) > .level-0 > span").should("be.visible").click();
+    cy.get(":nth-child(2) > .item > .product").should("be.visible").click();
+    cy.get(".outline-offset-2").should("be.visible").click();
+    cy.get(".text-gray-500").should("be.visible").click();
+    cy.get(".product-data > .action").should("be.visible").click();
+    cy.get("#customer-login-form > .fieldset > .actions-toolbar > .btn")
+      .should("be.visible")
       .click();
   });
 });
