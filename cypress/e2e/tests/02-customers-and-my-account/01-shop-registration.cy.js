54157ee35625f1f5a2d81960958730d8e605a40f
Daniel Viorel <daniel.viorel@arcmedia.ch>
2024-04-17 11:19:55 +0200

    update customer login

--- cypress/e2e/tests/02-customers-and-my-account/01-shop-registration.cy.js
+++ cypress/e2e/tests/02-customers-and-my-account/01-shop-registration.cy.js
@@ -16,9 +16,9 @@ describe("shop registration", function () {
 
     cy.get("#firstname").clear().type("testtest");
     cy.get("#lastname").clear().type("testdany");
-    cy.get('#street_1').type('test');
-    cy.get('#zip').type('6003');
-    cy.get('#city').type('Luzern');
+    cy.get("#street_1").type("test");
+    cy.get("#zip").type("6003");
+    cy.get("#city").type("Luzern");
     cy.get("#email_address").clear().type("testdany@test.com");
     cy.get("#password").clear().type("Test1234");
     cy.get("#password-confirmation").clear().type("Test1234");
@@ -26,10 +26,7 @@ describe("shop registration", function () {
     cy.visit(`${config.baseUrl}/customer/account/login/`);
     cy.get("#email").type("test@test.com");
     cy.get("#pass").type("Test1234");
-    cy.get(".fieldset > .actions-toolbar > .btn > span")
-      .should("be.visible")
-      .click();
-    cy.get("#customer-menu > .hidden").should("be.visible").click();
+    cy.get(".fieldset > .actions-toolbar > .btn > span").should("be.visible").click();
     cy.get("#customer-menu > .hidden").should("be.visible").click();
     cy.get(".items > :nth-child(2) > a").should("be.visible").click();
     cy.get(".items > :nth-child(3) > a").should("be.visible").click();
--- cypress/e2e/tests/02-customers-and-my-account/02-customer-login.cy.js
+++ cypress/e2e/tests/02-customers-and-my-account/02-customer-login.cy.js
@@ -1,62 +1,40 @@
 let config = Cypress.config();
 
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false
+Cypress.on("uncaught:exception", (err, runnable) => {
+  return false;
 });
 
-describe('Customer login', function () {
-
-  beforeEach('before each test', function () {
+describe("Customer login", function () {
+  beforeEach("before each test", function () {
     cy.visit(`${config.baseUrl}`);
-    cy.get('.ambar-btn-accept')
-      .should('be.visible')
-      .click();
+    cy.get(".ambar-btn-accept").should("be.visible").click();
     cy.wait(3000);
   });
 
-  it('login with a fake user', function () {
+  it("login with a fake user", function () {
     cy.visit(`${config.baseUrl}/customer/account/login/`);
-    cy.get('#email')
-      .type('erwtw@testtest.com')
-    cy.get('#pass')
-      .type('test1234')
-    cy.get('.fieldset > .actions-toolbar > .btn > span')
-      .should('be.visible')
+    cy.get("#email").type("erwtw@testtest.com");
+    cy.get("#pass").type("test1234");
+    cy.get(".fieldset > .actions-toolbar > .btn > span")
+      .should("be.visible")
       .click();
     cy.get("success-message").should("not.exist");
   });
 
-  it('login successfully', function () {
+  it("login successfully", function () {
     cy.visit(`${config.baseUrl}/customer/account/login/`);
-    
-    cy.get('#email')
-      .type('test@test.com');
-    cy.get('#pass')
-      .type('Test1234');
-    cy.get('.fieldset > .actions-toolbar > .btn > span')
-      .should('be.visible')
+
+    cy.get("#email").type("test@test.com");
+    cy.get("#pass").type("Test1234");
+    cy.get(".fieldset > .actions-toolbar > .btn > span")
+      .should("be.visible")
       .click();
-    cy.get('.items > :nth-child(2) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(3) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(4) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(5) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.items > :nth-child(6) > a')
-      .should('be.visible')
-      .click();
-    cy.get('.current > strong')
-      .should('be.visible')
-      .click();
-    cy.get(':nth-child(8) > a')
-      .should('be.visible')
-      .click();
+    cy.get(".items > :nth-child(2) > a").should("be.visible").click();
+    cy.get(".items > :nth-child(3) > a").should("be.visible").click();
+    cy.get(".items > :nth-child(4) > a").should("be.visible").click();
+    cy.get(".items > :nth-child(5) > a").should("be.visible").click();
+    cy.get(".items > :nth-child(6) > a").should("be.visible").click();
+    cy.get(".current > strong").should("be.visible").click();
+    cy.get(":nth-child(8) > a").should("be.visible").click();
   });
 });
-
